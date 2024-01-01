9f8f7780acee25a61f6568fc2e790d53e269146b
chinnaragamsetti <110217256+chinnaragamsetti@users.noreply.github.com>
2024-01-02 05:10:09 +0530

    Update index.js

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -1,55 +1,56 @@
 // Write your code here
 
+
 import {Component} from 'react'
 import './index.css'
 
 class Speedometer extends Component {
-    state = {speed: 0}
-      onAccelerate = () => {
-        const {speed} = this.state
-        if (speed < 200) {
-          this.setState(prevState => ({speed: prevState.speed + 10}))
-        }
-      }
-    
-      onBrake = () => {
-        const {speed} = this.state
-        if (speed > 0) {
-          this.setState(prevState => ({speed: prevState.speed - 10}))
-        }
-      }
+  state = {speed: 0}
 
-
-    render() {
-        const { speed } = this.state
-
-        return (
-        <div className="container">
-            <h1 className="mainheading">SPEEDOMETER</h1>
-
-            <img
-            alt="speedometer"
-            src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
-            className="image"
-            />
-
-            <p className="para1">Speed is {speed}mph</p>
-            <p className="para2">Min Limit is 0mph,Max Limit is 200mph</p>
-            <div className="buttonscontainer">
-            <button
-                type="button"
-                className="accelaratebutton"
-                onClick={this.onAccelerate}
-            >
-                Accelerate
-            </button>
-            <button type="button" className="brakebutton" onClick={this.onBrake}>
-                Apply Brake
-            </button>
-            </div>
-        </div>
-        )
+  onAccelerate = () => {
+    const {speed} = this.state
+    if (speed < 200) {
+      this.setState(prevState => ({speed: prevState.speed + 10}))
     }
+  }
+
+  onBrake = () => {
+    const {speed} = this.state
+    if (speed > 0) {
+      this.setState(prevState => ({speed: prevState.speed - 10}))
+    }
+  }
+
+  render() {
+    const {speed} = this.state
+
+    return (
+      <div className="container">
+        <h1 className="mainheading">SPEEDOMETER</h1>
+
+        <img
+          alt="speedometer"
+          src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
+          className="image"
+        />
+
+        <h1 className="para1">Speed is {speed}mph</h1>
+        <p className="para2">Min Limit is 0mph, Max Limit is 200mph</p>
+        <div className="buttonscontainer">
+          <button
+            type="button"
+            className="acceleratebutton"
+            onClick={this.onAccelerate}
+          >
+            Accelerate
+          </button>
+          <button type="button" className="brakebutton" onClick={this.onBrake}>
+            Apply Brake
+          </button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Speedometer
